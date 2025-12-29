c3cb753b63f3d3260329b8a17f1f6c2990d624c8
Dinesh0205-code <dineshvenkat0205@gmail.com>
2025-12-30 00:00:27 +0530

    adding files

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please add a product name"],
+    {
+        name: {
+            type: String,
+            required: [true, 'Please add a product name'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'Please add a price'],
+        },
+        description: {
+            type: String,
+            required: [true, 'Please add a description'],
+        },
+        image: {
+            type: String,
+            required: [true, 'Please add an image URL'],
+        },
+        category: {
+            type: String,
+            required: [true, 'Please add a category'],
+            enum: ['Wooden Frame', 'Polaroid Set'],
+        },
     },
-    price: {
-      type: Number,
-      required: [true, "Please add a price"],
-    },
-    description: {
-      type: String,
-      required: [true, "Please add a description"],
-    },
-    image: {
-      type: String,
-      required: [true, "Please add an image URL"],
-    },
-    category: {
-      type: String,
-      required: [true, "Please add a category"],
-      enum: ["frames", "polaroids"], // 👈 FIX
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model('Product', productSchema);
